6a66cf8f1be168ce9e44983cd37eb133b36bc79f
Graham Riches <graham.riches@live.com>
2021-08-29 10:45:14 -0600

    finish chapter on ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -7,6 +7,12 @@ fn main() {
     let s1 = gives_ownership();
     let s3 = takes_and_gives_back(s1);
     println!("{}", calculate_length(&s3));
+
+    let s4 = String::from("this is a string");
+    let index = find_first_word(&s4);
+    println!("{}", &s4[0..index]);
+    let first_word = find_first_word_as_slice(&s4);
+    println!("{}", first_word);
 }
 
 fn takes_ownership(some_string: String) {
@@ -28,4 +34,24 @@ fn takes_and_gives_back(a_string: String) -> String {
 
 fn calculate_length(a_string: &String) -> usize {
     a_string.len()
+}
+
+fn find_first_word(s: &String) -> usize{
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' '{
+            return i;
+        }
+    }
+    s.len()
+}
+
+fn find_first_word_as_slice(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' '{
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }

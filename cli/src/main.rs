e43ec644f82234f1a6a2dc7643148a54d6efa6f4
Graham Riches <graham.riches@live.com>
2021-10-10 16:34:09 -0600

    Started a disassembly parser to practice Rust with parsing and IO

---
+++ cli/src/main.rs
@@ -0,0 +1,29 @@
+#[macro_use]
+extern crate clap;
+use clap::{App};
+use arm_binary_tools::parser::symbol_table::SymbolTable;
+
+fn main() {
+    let matches = App::new("ARM Disassembly Parser")
+        .version("1.0.0")
+        .author("Graham Riches")
+        .about("Parses disassembled ARM binaries and provides useful output")
+        .args_from_usage(
+            "<INPUT>                'The disassembled binary file to parse'
+            -f, --functions         'Display only functions'
+            -s, --sort=[ORD]        'Sort the output. ORD='ascending', 'descending''
+            -v, --verbosity=[LEVEL] 'Enables verbose output with optional levels: <0,1,2>'")
+        .get_matches();
+
+    let file = matches.value_of("INPUT").unwrap();
+    let symbol_table = SymbolTable::from_file(file).expect("Could not parse input file as a symbol table");
+
+    let bss = symbol_table.into_iter()
+        .filter(|x| x.section == ".bss")
+        .collect::<SymbolTable>();
+    
+    print!("{:?}", bss);
+    
+
+
+}
